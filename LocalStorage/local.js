6a862a2392d12cbd288cd2ce0233359b10f03304
malikdastan <maliktabish369@gmail.com>
2024-02-06 13:49:41 +0530

    Project Sharpeners

--- LocalStorage/local.js
+++ LocalStorage/local.js
@@ -1,23 +1,32 @@
 function handleFormSubmit(event){
     event.preventDefault();
-    const name = event.target.username.value;
-    const email = event.target.email.value;
-    const phone = event.target.phone.value;
+    const expense = event.target.expense.value;
+    const description = event.target.description.value;
+    const category = event.target.category.value;
 
-    const userDetails = {
-        username: name,
-        email: email,
-        phone: phone
+    const expenseDetails = {
+        expense: expense,
+        description: description,
+        categpry: category
     }
 
-    localStorage.setItem(userDetails.email, JSON.stringify(userDetails));
-    displayUser(userDetails);
+    localStorage.setItem(expenseDetails.description, JSON.stringify(expenseDetails));
+
+    /*Reset form fields 
+    to empty values*/
+
+    event.target.expense.value = '';
+    event.target.description.value = '';
+    event.target.category.value = '';
+
+
+    displayUser(expenseDetails);
 }
 
-function displayUser(userDetails){
+function displayUser(expenseDetails){
     const list = document.getElementsByTagName("ul")[0];
     const newUser = document.createElement('li');
-        newUser.textContent = userDetails.username + " " + userDetails.email +" "+ userDetails.phone;
+        newUser.textContent = `${expenseDetails.expense} ${expenseDetails.description} ${expenseDetails.category}`;
 
         /*creating delete 
         button*/
@@ -26,13 +35,38 @@ function displayUser(userDetails){
         deleteBtn.type = "button";
         deleteBtn.textContent = ("Delete");
         deleteBtn.onclick = function(){
-            localStorage.removeItem(userDetails.email);
+            localStorage.removeItem(expenseDetails.email);
             list.removeChild(newUser);
         }
-        /*adding delete button
+
+        /*creating Edit button */
+
+        const editBtn = document.createElement('button');
+        editBtn.type = "button";
+       editBtn.textContent = ("Edit");
+       editBtn.onclick = function(){
+        list.removeChild(newUser);
+            
+            const text = document.getElementById('expense');
+            const email = document.getElementById('description');
+            const phone = document.getElementById('category');
+
+            const storedDetails = JSON.parse(localStorage.getItem(expenseDetails.description));
+            if (storedDetails){
+                text.value = storedDetails.expense;
+                email.value = storedDetails.description;
+                phone.value = storedDetails.category;
+            }
+            localStorage.removeItem(expenseDetails.description);
+            
+        }
+
+        /*adding delete button & 
+        Edit button 
         to the list item*/
 
         newUser.appendChild(deleteBtn);
+        newUser.appendChild(editBtn);
         list.appendChild(newUser);
         console.log (deleteBtn);
         console.log(list);
